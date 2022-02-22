6e06cac3949985ebef1a477327a4caca8b35d9a3
Pratik Bhadane <pratikbhadane24@gmail.com>
2022-02-22 12:35:53 +0530

    added fetch_user

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import hmac
+import hashlib
+import json
 import time
+import requests
 import pandas as pd
 
 key = ""  # Your API Key
@@ -14,8 +18,33 @@ limit = "100"
 
 
 class CoinDCX():
-    pass
+
+    def login(self, endpoint):
+        secret_bytes = bytes(secret, encoding='utf-8')
+        json_body = json.dumps(self.body, separators=(',', ':'))
+        signature = hmac.new(secret_bytes, json_body.encode(),
+                             hashlib.sha256).hexdigest()
+        url = f"{api_end_point}{endpoint}"
+
+        self.headers = {
+            'Content-Type': 'application/json',
+            'X-AUTH-APIKEY': key,
+            'X-AUTH-SIGNATURE': signature
+        }
+        response = requests.post(
+            url, data=json_body, headers=self.headers)
+        data = response.json()
+        return data
+
+    def user_details(self):
+        self.body = {"timestamp": timeStamp}
+        endpoint = "exchange/v1/users/info"
+        data = self.login(endpoint)
+        print(
+            f"Logged in as: {data['first_name']} {data['last_name']}\nUserID: {data['coindcx_id']}")
+        self.fname = data['first_name']
+        self.lname = data['last_name']
 
 
 user = CoinDCX()
-
+user.user_details()
